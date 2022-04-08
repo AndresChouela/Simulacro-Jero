171698daa18d5b92babbbde63fa9fbb5d8727665
AndresChouela <101569013+AndresChouela@users.noreply.github.com>
2022-04-08 08:42:03 -0300

    Initial commit

---
+++ Assets/Scenes/Ejercicio1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour {
+    public int num1;
+    public int num2;
+	// Use this for initialization
+	void Start () {
+		if (num1 == num2)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+        else
+        {
+            Debug.Log("Los numeros no son iguales");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scenes/Ejercico2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercico2 : MonoBehaviour {
+    public int precio1;
+    public int precio2;
+    public int precio3;
+    public int montodinero;
+    // Use this for initialization
+    void Start () {
+        int preciototal = precio1 + precio2 + precio3;
+        int sobra;
+        if (montodinero > preciototal)
+        {
+            sobra = montodinero - preciototal;
+            Debug.Log("El monot de dinero es superior al precio de los productos y sobran $" + sobra);
+        }
+        else if (preciototal > montodinero)
+        {
+            sobra = preciototal - montodinero;
+            Debug.Log("El monot de dinero no supera al precio de los productos y faltan $" + sobra);
+        }
+        else
+        {
+            Debug.Log("El monot de dinero es igual al precio de los productos");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
